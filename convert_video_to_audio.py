731f9a5be04686b6fd55c223388481f686eac3cf
Jirawat S <57433713+JRWSP@users.noreply.github.com>
2023-11-17 20:50:59 +0100

    Now specify output to be 16kHz .wav

--- convert_video_to_audio.py
+++ convert_video_to_audio.py
@@ -2,12 +2,13 @@ import os
 import argparse
 from moviepy.editor import VideoFileClip
 
-def convert_video_to_audio_moviepy(video_file, output_ext="mp3"):
-    """Converts video to audio using MoviePy library
-    that uses `ffmpeg` under the hood"""
+def convert_video_to_audio_moviepy(video_file, output_ext="wav"):
+    """
+    Converts video to audio 16KHz .wav using MoviePy library that uses `ffmpeg` under the hood.
+    """
     filename, ext = os.path.splitext(video_file)
     clip = VideoFileClip(video_file)
-    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+    clip.audio.write_audiofile(f"{filename}.{output_ext}", codec='pcm_s16le', fps=16000)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Extract mp3 from video.")
